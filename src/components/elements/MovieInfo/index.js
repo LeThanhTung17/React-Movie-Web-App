81f69ec040f2351b2bd8255fbfd401adc6c22236
Le Thanh Tung <42868337+LeThanhTung17@users.noreply.github.com>
2020-06-28 17:11:28 +0700

    Update component MovieInfo

--- src/components/elements/MovieInfo/index.js
+++ src/components/elements/MovieInfo/index.js
@@ -5,7 +5,55 @@ import MovieThumb from "../MovieThumb";
 import "./MovieInfo.css";
 
 const MovieInfo = (props) => {
-  return <div>MovieInfo</div>;
+  return (
+    <div
+      className="rmdb-movieinfo"
+      style={{
+        background: props.movie.backdrop_path
+          ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}')`
+          : "#000",
+      }}
+    >
+      <div className="rmdb-movieinfo-content">
+        <div className="rmdb-movieinfo-thumb">
+          <MovieThumb
+            image={
+              props.movie.backdrop_path
+                ? `${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}`
+                : "./images/no_image.jpg"
+            }
+            clickable={false}
+          />
+        </div>
+        <div className="rmdb-movieinfo-text">
+          <h1>{props.movie.title}</h1>
+          <h3>PLOT</h3>
+          <p>{props.movie.overview}</p>
+          <h3>IMDB RATING</h3>
+          <div className="rmdb-rating">
+            <meter
+              min="0"
+              max="100"
+              optimum="100"
+              low="40"
+              high="70"
+              value={props.movie.vote_average * 10}
+            ></meter>
+            <p className="rmdb-score">{props.movie.vote_average}</p>
+          </div>
+          {props.movie.directors > 1 ? <h3>Directors</h3> : <h3>Director</h3>}
+          {props.movie.directors.map((el, i) => {
+            return (
+              <p className="rmdb-director" key={i}>
+                {el.name}
+              </p>
+            );
+          })}
+        </div>
+        <FontAwesome className="fa-film" name="film" size="5x" />
+      </div>
+    </div>
+  );
 };
 
 export default MovieInfo;
